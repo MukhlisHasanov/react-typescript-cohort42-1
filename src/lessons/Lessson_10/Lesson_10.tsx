235e4bd0e16d235cf17e0cd12397badb75a081f8
ilofrudd <boyko5000@gmail.com>
2024-10-05 19:51:49 +0200

    Working project without styles

--- src/lessons/Lessson_10/Lesson_10.tsx
+++ src/lessons/Lessson_10/Lesson_10.tsx
@@ -1,19 +1,76 @@
 import Button from "components/Button/Button";
 import Input from "components/Input/Input";
+import { PageWrapper, Card, Text } from "./styles";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { ApiUniversity, University } from './types';
 
-import { PageWrapper } from "./styles"
+function Lesson_10() {
+  const [country, setCountry] = useState<string>("");
+  const [universities, setUniversities] = useState<University[]>([]);
+  const [error, setError] = useState<string | undefined>(undefined);
 
-function Lesson_10 () {
-    return (
-        <PageWrapper>
-        <Input
-        $error={undefined}
+  const onInputCountry = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setCountry(event.target.value);
+  };
+
+  const getUniversities = async (): Promise<void> => {
+    if (!country.trim()) {
+      alert("Please, enter a country name.");
+      return;
+    }
+    try {
+      const response: Response = await fetch(
+        `http://universities.hipolabs.com/search?country=${encodeURIComponent(
+          country
+        )}`,
+        { method: "GET" }
+      );
+
+      if (!response.ok) {
+        setError("Error fetching data from the server.");
+        return;
+      }
+
+      const result: ApiUniversity[] = await response.json();
+
+      const firstFifteenUniversities = result.slice(0, 15);
+
+      const universitiesWithId: University[] = firstFifteenUniversities.map(
+        (university: ApiUniversity) => ({
+          ...university,
+          id: uuidv4(),
+        })
+      );
+      setUniversities(universitiesWithId);
+      setError(undefined);
+    } catch (error) {
+      console.error("Network Error", error);
+      setError("Network Error");
+    }
+  };
+
+  return (
+    <PageWrapper>
+      <Input
         id="1"
         label="Country"
-        placeholder="Enter Country for searching universities"
+        placeholder="Enter country for searching universities"
         name="University"
-      />       
-      </PageWrapper>
-    )
+        value={country}
+        onChange={onInputCountry}
+      />
+      <Button name="Get Universities" onClick={getUniversities} />
+      {error && <Text>{error}</Text>}
+      {universities.length > 0 && (
+        <Card>
+          {universities.map((university: University) => (
+            <Text key={university.id}>{university.name}</Text>
+          ))}
+        </Card>
+      )}
+    </PageWrapper>
+  );
 }
-export default Lesson_10
+
+export default Lesson_10;
